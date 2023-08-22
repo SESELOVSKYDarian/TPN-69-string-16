34a1ef07952292decfe4af6112580fd8f90fcd04
SESELOVSKYDarian <133694056+SESELOVSKYDarian@users.noreply.github.com>
2023-08-22 12:44:10 -0300

    Add files via upload

---
+++ TP69.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string InsertarBarraBaja(string f);
+int main()
+{
+    string frase;
+    cout<<"Ingrese frase: ";
+    getline(cin, frase);
+    cout<<InsertarBarraBaja(frase)<<endl;
+    return 0;
+}
+
+string InsertarBarraBaja(string f)
+{
+    string s="_";
+    for (int i=i; i<f.size(); i++)
+    {
+        if (f[i]==' ')
+        {
+           f.erase (i, 1);
+           f.insert (i, s);
+           i++;
+        }
+    }
+    return f;
+}
